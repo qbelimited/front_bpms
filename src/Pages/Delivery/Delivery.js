48a6e691fc4f9c5f50e4c64c648d2b5817016a75
inioluwadavid <ajidaveini@gmail.com>
2022-10-26 11:33:48 +0100

    completed delivery page

--- src/Pages/Delivery/Delivery.js
+++ src/Pages/Delivery/Delivery.js
@@ -1,8 +1,20 @@
 import React from 'react'
+import DeliveryTable from '../../Components/Delivery/DeliveryTable'
 
 function Delivery() {
   return (
-    <div>Delivery</div>
+    <div>
+      <div>
+            <h1 className=' font-bold text-start '>Delivery</h1>
+            <p className=' text-gray-400 text-start'>Delivery information page - Please edit to fit proper descriprion of the page</p>
+        </div>
+        <div className=' mt-10 grid md:grid-cols-5'>
+        <div className=' col-span-4'>
+        <DeliveryTable />
+        </div>
+         
+        </div>
+    </div>
   )
 }
 
